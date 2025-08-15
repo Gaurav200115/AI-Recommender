c25937a24f2cb3134abe3ff19cb26e8646107545
Gaurav200115 <76817030+Gaurav200115@users.noreply.github.com>
2025-08-15 22:24:59 +0530

    New Changes

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.use(express.json());
 // Routes
 app.use('/', recommendationRoutes);
 
+app.get('/testing', (req, res) => {
+  res.send('Hello World');
+});
+
 app.listen(3000, () => console.log('✅ Backend running on port 3000'));
